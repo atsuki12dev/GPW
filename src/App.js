2249f87d1130509c5e0091ada05d026fc5dec9b4
KioLissMik <koteikabro889@gmail.com>
2025-02-12 16:18:36 +0500

    EmptyProject
    Start project...

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './global_scss/App.scss';
+
+import MazitovAA from './gpw-blocks/MazitovAA';
+
+function App() {
+  return (
+    <div className="container">
+      <MazitovAA />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/gpw-blocks/MazitovAA/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './MazitovAA.module.scss';
+
+function MazitovAA() {
+  return (
+    <div className={styles.root}>
+        текст
+    </div>
+  )
+}
+export default MazitovAA;
